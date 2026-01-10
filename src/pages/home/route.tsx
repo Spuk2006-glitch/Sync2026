9580ed3fe7e3e683d93f7b0ef88e057b164c973c
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-01-10 08:20:47 +0530

    Initial commit

--- src/pages/home/route.tsx
+++ src/pages/home/route.tsx
@@ -1,7 +1,8 @@
 import { Dashboard } from "./sections/dashboard/Dashboard"
+import WhySection from "./sections/whySection/WhySection"
 
 export const HomeRoute = () => {
 	return (
-		<Dashboard />
+		<WhySection/>
 	)
 }
---
+++ src/pages/home/sections/whySection/WhySection.tsx
@@ -0,0 +1,14 @@
+import "./WhySection.css";
+
+const WhySection = () => {
+    return (
+        <section className="whySection">
+            <h1 className="whyTitle">
+            Why you should join Synchronocity
+            </h1>
+        </section>
+    );
+  };
+  
+  export default WhySection;
+  
