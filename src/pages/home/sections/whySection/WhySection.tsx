9fc04303d73b0b1a8e762d37b5f6549ea483741e
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-01-12 12:38:00 +0530

    WhySectionCSS

--- src/pages/home/sections/whySection/WhySection.tsx
+++ src/pages/home/sections/whySection/WhySection.tsx
@@ -2,11 +2,15 @@ import "./WhySection.css";
 
 const WhySection = () => {
     return (
-        <section className="whySection">
-            <h1 className="whyTitle">
-            Why you should join Synchronocity
-            </h1>
-        </section>
+        <div className="page">
+            <div className="whySection">
+                <h1 className="whyTitle">
+                Why you should <br/>
+                join ACM?
+                </h1>
+            </div>
+        </div>
+        
     );
   };
   
